4aa31ab13dcb9f36df46055eff60a00ca7efabba
LuisAlejandroCortesGalan <alejandro.cortes.bcn@outlook.comgit config --global user.name LuisAlejandroCortesGalan>
2024-06-28 11:38:26 +0200

    asi es como lo presentaremos, ya tiene solucionado el tema de links del header

--- public/js/header.js
+++ public/js/header.js
@@ -1,25 +1,27 @@
 // Función para alternar el estilo display del menú principal
 function toggleMenu() {
     const navUl = document.querySelector('nav ul');
-
-    // Verificar el estado actual del menú
     const isMenuVisible = navUl.style.display === 'flex';
-
-    // Cambiar el estilo display del menú
-    if (isMenuVisible) {
-        navUl.style.display = 'none'; // Ocultar el menú
-    } else {
-        navUl.style.display = 'flex'; // Mostrar el menú cambiando el display a flex
-    }
+    navUl.style.display = isMenuVisible ? 'none' : 'flex';
 }
+
 // Evento para cerrar el menú al hacer clic fuera de él y en dispositivos táctiles
 document.addEventListener('click', function(event) {
     const nav = document.querySelector('nav');
     const menuToggle = document.querySelector('.menu-toggle');
-    
     if (!nav.contains(event.target) && nav.classList.contains('show')) {
         nav.classList.remove('show');
     }
+});
+
+// EVITO LA PROPAGACIÓN DEL EVENTO AL CLICK EN LOS ENLACES DEL MENU
+document.addEventListener("DOMContentLoaded", function(){
+    var listItems = document.querySelectorAll(".dropdown-menu li");
+    listItems.forEach(function(item){
+      item.addEventListener("click", function(event){
+        event.stopPropagation(); // Detiene la propagación del evento al elemento padre
+      });
+    });
 });
 
 // Eventos para abrir y cerrar los menús desplegables en dispositivos móviles y de escritorio
@@ -35,14 +37,14 @@ dropdownItems.forEach(item => {
     });
 
     // Evento para mostrar el menú desplegable al hacer hover en dispositivos de escritorio
-    item.addEventListener('mouseenter', function(event) {
+    item.addEventListener('mouseenter', function() {
         if (window.innerWidth > 768) { // Dispositivo de escritorio
             item.classList.add('open'); // Agregar la clase 'open' para mostrar el menú desplegable
         }
     });
 
     // Evento para ocultar el menú desplegable al dejar de hacer hover en dispositivos de escritorio
-    item.addEventListener('mouseleave', function(event) {
+    item.addEventListener('mouseleave', function() {
         if (window.innerWidth > 768) { // Dispositivo de escritorio
             item.classList.remove('open'); // Quitar la clase 'open' para ocultar el menú desplegable
         }
